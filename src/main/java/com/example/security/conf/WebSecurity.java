46ca7e69b0e72805d7247c2c21ab0367b5a5bc99
Mauricio Sánchez <mauriciosanchez@worldoffice.com.co>
2022-04-25 07:55:26 -0500

    d

--- src/main/java/com/example/security/conf/WebSecurity.java
+++ src/main/java/com/example/security/conf/WebSecurity.java
@@ -1,7 +1,6 @@
 package com.example.security.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import com.example.security.conf.jwt.JWTAuthenticationFilter;
 import com.example.security.conf.jwt.JWTAuthorizationFilter;
 
@@ -47,13 +44,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+//		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 		auth.authenticationProvider(authenticationProvider);
-	}
-	
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 }
